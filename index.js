44c30d3d4ca07fd024e91679aee015de32d93fb2
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-05-28 19:03:51 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let computer_number; // numero gerado pelo computador
-let user_numbers = []; // array para armazenar os numeros digitados pelo usuario
-let attempts = 0; // numero de tentativas
-let max_guesses = 10; // numero maximo de tentativas
+let computer_number;
+let user_numbers = [];
+let attempts = 0;
+let max_guesses = 10;
 
 function newGame() {
     window.location.reload();
 }
 
 function init() {
-    computer_number = Math.floor(Math.random() * 100 + 1); // gera numero randomico e arrendonda com math.floor
+    computer_number = Math.floor(Math.random() * 100 + 1);
 }
 
 function compareNumbers() {
-    const user_number = Number(document.getElementById('input-box').value); // recebe o numero digitado pelo usuario
+    const user_number = Number(document.getElementById('input-box').value);
 
     if (user_number < 1 || user_number > 100) {
         document.getElementById('text-output').innerHTML = 'Enter a valid number between 1 and 100';
-        document.getElementById('input-box').value = ''; // zera o campo de digitaçao
+        document.getElementById('input-box').value = '';
         return;
     }
 
-    user_numbers.push(' ' + user_number); // armazena o numero no array
-    document.getElementById('guesses').innerHTML = user_numbers; // retorna array nos palpites
+    user_numbers.push(' ' + user_number);
+    document.getElementById('guesses').innerHTML = user_numbers;
 
     if (attempts < max_guesses) {
         if (user_number > computer_number) {
             document.getElementById('text-output').innerHTML = 'Your number is too high';
-            document.getElementById('input-box').value = ''; // zera o campo de digitaçao
-            attempts++; // adiciona 1 ao numero de tentativas
+            document.getElementById('input-box').value = '';
+            attempts++;
             document.getElementById('attempts').innerHTML = attempts;
         } else if (user_number < computer_number) {
             document.getElementById('text-output').innerHTML = 'Your number is too low';
-            document.getElementById('input-box').value = ''; // zera o campo de digitaçao
-            attempts++; // adiciona 1 ao numero de tentativas
+            document.getElementById('input-box').value = '';
+            attempts++;
             document.getElementById('attempts').innerHTML = attempts;
         } else {
             document.getElementById('text-output').innerHTML = 'Congratulations!!!';
-            attempts++; // adiciona 1 ao numero de tentativas
+            attempts++;
             document.getElementById('attempts').innerHTML = attempts;
-            document.getElementById('input-box').setAttribute('Readonly', 'Readonly'); // para a aplicaçao
+            document.getElementById('input-box').setAttribute('Readonly', 'Readonly');
         }
     } else {
         document.getElementById('text-output').innerHTML = 'You lose!!! The computer number was: ' + computer_number;
-        document.getElementById('input-box').setAttribute('Readonly', 'Readonly'); // para a aplicaçao
+        document.getElementById('input-box').setAttribute('Readonly', 'Readonly');
     }
 }
