3585a705a8159e95e70ff90779dbcb40f9062d91
Rafaella Trevizan <116769491+rafinhatrevs@users.noreply.github.com>
2024-06-24 17:44:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-let computer_number;
-let user_numbers = [];
+let computerNumber;
+let userNumbers = [];
 let attempts = 0;
-let max_guesses = 10;
+let maxGuesses = 10;
 
 function newGame() {
     window.location.reload();
 }
 
 function init() {
-    computer_number = Math.floor(Math.random() * 100 + 1);
+    computerNumber = Math.floor(Math.random() * 100 + 1);
 }
 
 function compareNumbers() {
-    const user_number = Number(document.getElementById('input-box').value);
+    const userNumber = Number(document.getElementById('input-box').value);
 
-    if (user_number < 1 || user_number > 100) {
+    if (userNumber < 1 || userNumber > 100) {
         document.getElementById('text-output').innerHTML = 'Enter a valid number between 1 and 100';
         document.getElementById('input-box').value = '';
         return;
     }
 
-    user_numbers.push(' ' + user_number);
-    document.getElementById('guesses').innerHTML = user_numbers;
+    userNumbers.push(' ' + userNumber);
+    document.getElementById('guesses').innerHTML = userNumbers;
 
-    if (attempts < max_guesses) {
-        if (user_number > computer_number) {
+    if (attempts < maxGuesses) {
+        if (userNumber > computerNumber) {
             document.getElementById('text-output').innerHTML = 'Your number is too high';
             document.getElementById('input-box').value = '';
             attempts++;
             document.getElementById('attempts').innerHTML = attempts;
-        } else if (user_number < computer_number) {
+        } else if (userNumber < computerNumber) {
             document.getElementById('text-output').innerHTML = 'Your number is too low';
             document.getElementById('input-box').value = '';
             attempts++;
@@ -41,7 +41,7 @@ function compareNumbers() {
             document.getElementById('input-box').setAttribute('Readonly', 'Readonly');
         }
     } else {
-        document.getElementById('text-output').innerHTML = 'You lose!!! The computer number was: ' + computer_number;
+        document.getElementById('text-output').innerHTML = 'You lose!!! The computer number was: ' + computerNumber;
         document.getElementById('input-box').setAttribute('Readonly', 'Readonly');
     }
 }
